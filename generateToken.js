f1c6e64acee701baf3f90aac0f0cef1154ba040f
Dhiraj-Nishad <dhirajnishad29@gmail.com>
2025-04-27 20:49:21 +0530

    Removed sensitive information

--- generateToken.js
+++ generateToken.js
@@ -1,7 +1,8 @@
 const { google } = require("googleapis");
+const dotenv = require("dotenv").config()
 
-const CLIENT_ID = "841273180638-dc30p3onqnosbojc889e1kmapiija9l3.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URI = "http://localhost:3000";
 
 const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
@@ -17,8 +18,7 @@ const url = oauth2Client.generateAuthUrl({
 console.log("Authorize this app by visiting this URL:", url);
 
 // After visiting the URL, paste the authorization code here
-const code = "4/0Ab_5qlnH06wB5LMjaLAJTEo_CJZd8MjnF_aHiTuA-uv2xLbCmvnvRCA6naGSBgrHjcssqw";
-
+const code = process.env.CODE
 oauth2Client.getToken(code, (err, token) => {
     if (err) return console.error("Error retrieving access token", err);
     console.log("Refresh Token:", token.refresh_token);
